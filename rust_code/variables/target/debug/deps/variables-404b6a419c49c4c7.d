4f79407ec5a2b9ca40d911f708214d9112176ea2
wenjin1997 <wenjin1997@126.com>
2022-05-31 17:04:32 +0800

    增加 代码

---
+++ rust_code/variables/target/debug/deps/variables-404b6a419c49c4c7.d
@@ -0,0 +1,5 @@
+/Users/jinjin/code/rust-notes/rust_code/variables/target/debug/deps/variables-404b6a419c49c4c7: src/main.rs
+
+/Users/jinjin/code/rust-notes/rust_code/variables/target/debug/deps/variables-404b6a419c49c4c7.d: src/main.rs
+
+src/main.rs:
---
+++ rust_code/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/jinjin/code/rust-notes/rust_code/variables/target/debug/variables: /Users/jinjin/code/rust-notes/rust_code/variables/src/main.rs
